0497d87a94bd68a10e5a03452135f774789357f5
Jakub Mokrejš <97549589+kmokrejs@users.noreply.github.com>
2023-05-19 10:40:22 +0200

    Update cameraControl.py
    Added description and comments on all funcitons

--- cameraControl.py
+++ cameraControl.py
@@ -3,16 +3,15 @@ import datetime
 import cv2 as cv
 
 class CamControl:
-
+    
+    # Initializes the class
     def __init__(self):
         pass
-
+    
+    # Detects the camera connected and returns their names and ports
     def detect_camera(self):
-
             cmd = [ 'gphoto2', '--auto-detect']
-
-            output = subprocess.check_output(cmd)
-                    
+            output = subprocess.check_output(cmd)    
             output = output.decode('utf-8')
             lines = output.splitlines()
 
@@ -25,31 +24,27 @@ class CamControl:
             camera_ports = []
 
             for line in camera_lines:
-                # Rozdělení řádku na části podle mezer
                 parts = line.split()
-
-                # Získání názvu kamery a portu
                 name = ' '.join(parts[:-1])
                 port = parts[-1]
-                    # Přidání názvu kamery a portu do příslušných seznamů
                 camera_names.append(name)
                 camera_ports.append(port)
 
             return camera_names, camera_ports
 
-
+    # Gets the settings for a specific port and setting
     def get_settings(self, port, settings=str):
+            # Input: port (str) - the port where the camera is connected
+            # settings (str) - the settings to get from the camera
+            
             cmd = [ 'gphoto2', '--port', port, '--get-config='+settings]
-
             result = subprocess.check_output(cmd)
-
             dsettings_values=[]
 
             for line in result.decode('utf-8').split('\n'):
                 if line.startswith('Choice:'):
                     parts = line.split()
                     temp = []
-                    #if settings has more than 1 word, join them to one string
                     for part in range(2,len(parts)):
                         temp.append(parts[part])
                     
@@ -58,13 +53,13 @@ class CamControl:
 
             return dsettings_values
     
-
+    # Gets the current settings for a specific port and setting
     def current_settings(self, port, settings=str):
-
+        # Input: port (str) - the port where the camera is connected
+        # settings (str) - the settings to get from the camera
+        
         cmd = [ 'gphoto2', '--port', port, '--get-config='+settings]
-
         result = subprocess.check_output(cmd)
-
         current_settings = ''
 
         for line in result.decode('utf-8').split('\n'):
@@ -79,54 +74,52 @@ class CamControl:
 
         return current_settings
     
-
+    # Sets a specific setting for a specific port
     def set_settings(self, port, settings=str, settings_value=str):
-
+        # Input: port (str) - the port where the camera is connected
+        # settings (str) - the settings to set on the camera
+        # settings_value (str) - the value to set for the settings
+        
         cmd = [ 'gphoto2', '--port', port, '--set-config', settings+'='+settings_value]
-
         subprocess.run(cmd)
 
-
+    # Takes a picture and saves it to a specific path with a specific name
     def take_picture(self, path=str, photo_name=str):
-        date = datetime.datetime.now()
+        # Input: path (str) - the path where the photo will be saved
+        # photo_name (str) - the name to give the photo
         
+        date = datetime.datetime.now()
         now = [str(date.day), str(date.month), str(date.microsecond)]
-
         now = '_'.join(now)
-
         cmd = [ 'gphoto2', '--capture-image-and-download', '--filename', path+'/'+photo_name+'_'+now]
 
         subprocess.run(cmd)
 
+    # Takes a picture in HDR and saves it to a specific path with a specific name
     def take_picture_hdr(self, path=str, photo_name=str):
-
+        # Input: path (str) - the path where the photo will be saved
+        # photo_name (str) - the name to give the photo
+        
         cmd = [ 'gphoto2', '--capture-image-and-download', '--filename', path+'/'+photo_name]
-
         subprocess.run(cmd)
 
+    # Displays a live view from a specific camera 
     def live_view(self, camera_num):
+        # Input: camera_num (int) - the number of the camera to view
+        
         cap = cv.VideoCapture(camera_num)
-
-        # Check if the webcam is opened correctly
         if not cap.isOpened():
             raise IOError("Cannot open webcam")
 
         while True:
             ret, frame = cap.read()
             if frame is None:
-                print("Snímek nenalezen. Zkontrolujte index kamery nebo připojení kamery.")
+                print("Picture not found. Chceck camera index or camera connection.")
                 break
             frame = cv.resize(frame, None, fx=0.2, fy=0.2, interpolation=cv.INTER_AREA)
             cv.imshow('Live View', frame)
-
             c = cv.waitKey(1)
             if c == 27:
                 break
-
         cap.release()
-        cv.destroyAllWindows()
-
-# to-do: picturestyle, capture-movie <78s...>
-
-    
-
+        cv.destroyAllWindows()  
